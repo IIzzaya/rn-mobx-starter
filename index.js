82db3b59b66381230d514a23e11256d1f8437599
IIzzaya <linxuan_0709@icloud.com>
2018-04-16 14:40:58 +0800

    Remove a warning of deprecated method

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry,YellowBox } from 'react-native';
 import App from './src/root';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 AppRegistry.registerComponent('rn-mobx-starter', () => App);
